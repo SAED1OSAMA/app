ea3738a83fe47babf60ecf3d3278908e972c5081
SAED1OSAMA <134612936+SAED1OSAMA@users.noreply.github.com>
2023-05-25 17:03:02 +0300

    Add files via upload

---
+++ cop.cpp
@@ -0,0 +1,225 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+
+
+
+int main()
+{
+ //=====================================///1  _1مشروع 
+
+/*cout <<"========================\n"; 
+cout <<"============SAED========\n";
+cout <<"========================\n";
+int spa;
+cin >> spa;
+
+int spa_in_days = spa * 365;
+int spa_in_hours = spa_in_days * 24;
+int spa_in_minutes = spa_in_hours * 60;
+int spa_in_seconds = spa_in_minutes * 60;
+
+cout <<"days " << spa_in_days;
+cout <<"\n hours " << spa_in_hours;
+cout <<"\n minutes " << spa_in_minutes;
+cout <<"\n seconds " << spa_in_seconds;
+//==========================================///2
+
+int num = 20;
+string name = "saed";
+bool status = true;
+
+int num_sad = 200;
+int num_sdd = 12;
+
+cout << sizeof(num) << "\n";
+cout << sizeof(name) << "\n";
+cout << sizeof(status) << "\n";
+
+cout << num_sad / num_sdd;
+//=====================================///3 _2مشروع
+
+int sed;
+cin >> sed;
+
+int sed_in_kilobytes = sed * 1024;
+int sed_in_bytes = sed_in_kilobytes * 8;
+cout << "bytes=";
+cout << sed_in_kilobytes;
+cout << "\nbyts=";
+cout << sed_in_bytes;
+//====================================///4
+
+int tre = 30; 
+tre = 56.7;
+cout << sizeof(tre) << endl;
+cout << tre << endl;
+
+
+double ert = 20;
+ert = 20.55;
+cout << sizeof(ert) << endl; 
+cout << ert << endl;
+
+
+float fl = 10.45f + 47.87f - 11.2f;
+cout << sizeof(fl) << endl;
+cout << fl << endl;
+//==============================///5 _3مشروع
+
+char sdf;
+cin >> sdf;
+cout << "hi:::" << int(sdf);
+
+
+int ast;
+cin >> ast;
+cout << "\nhi:::" << char(ast);
+//==============================///6
+
+int opn = 400;
+cout << sizeof(opn) << "\n"; //4 Bytes
+
+short int opn_e = 400;
+cout << sizeof(opn_e) << "\n"; //2 Bytes
+
+short opn_r = 400;
+cout << sizeof(opn_r) << "\n"; //2 Bytes
+
+cout << sizeof(short int) << "\n"; //2
+cout << sizeof(short) << "\n"; //2
+cout << sizeof(long int) << "\n"; //4
+cout << sizeof(long) << "\n"; //4
+cout << sizeof(long long int) << "\n"; //8
+cout << sizeof(long long) << "\n"; //8
+cout << "===================\n";
+
+int pon_t = -200;
+cout << pon_t << "\n";
+
+signed pon_u = -100;
+cout << pon_u << "\n";
+
+unsigned pon_o = 200;
+cout << pon_o << "\n";
+
+unsigned  num_four = -10; // مشكلة مش راح يطلع القيمة التي هي بسالب فقط الموجب 
+cout << num_four << "\n";
+
+
+using fil = long int; // طريقة 1
+typedef long int fil; //طريقة 2
+fil fl = 100023;
+cout << fl << "\n";
+
+int f = 20.5;
+double g ;
+g = f;
+cout << g << "\n";
+cout << sizeof(g) << "\n";//8
+cout << "---------------------------\n";
+char c = 'c';
+int d = 67;
+cout << int(c) << "\n";//99
+cout << c + d << "\n";//166
+cout << "---------------------------\n";
+int u = 56.9;
+double n = 67; 
+cout << u + n << "\n";
+cout << sizeof(u+n) << "\n"; 
+
+int h = 56.9;
+double a = 67; 
+cout << h + (int)a << "\n";
+cout << sizeof(h + (int)a) << "\n"; 
+
+int z = 56.9;
+double w = 67; 
+cout << z + int(w) << "\n";
+cout << sizeof(z + int(w)) << "\n"; 
+//====================================////8
+cout << 15 + 10 << "\n";
+cout << sizeof(15+10) <<"\n";
+cout << 12.5 + 14.5 <<"\n";
+cout << sizeof(12.5+14.5) <<"\n";
+cout << int (150.5 + 520.5) << "\n";
+cout << sizeof( 150.5f +520.5f) << "\n";
+cout << sizeof(int(14.5+16.5)) << "\n";
+
+cout << 45 - 63 << "\n";
+cout << 12 - -4 << "\n";
+
+cout << 50 * 10 << "\n ";
+
+cout << 100 / 4 << "\n";
+cout << 22 / 5 << "\n";
+cout << 22.f / 5 << "\n"; 
+
+cout << 60 % 6 << "\n";
+cout << 55 % 6 << "\n";
+cout << 41 % 4 << "\n";
+//==========================///9 
+
+cout << 10 % 2 << "\n";
+cout << 50 % 2 << "\n";
+cout << 12 % 2 << "\n";
+cout << 11 % 2 << "\n";
+
+int a = 23;
+//a = a - 12; // a= 23 - 12 = 11
+a -= 12; // a= 23 - 12 =11
+cout << a << "\n";
+
+int w = 45 ;
+//w = w + 10 ; // w = 45 + 10 = 55
+w += 10; // w = 45 + 10 =55
+cout << w << "\n";
+
+int s = 60 ;
+cout << s << "\n";
+//s = s * 2; // s = 60 * 2 = 120
+s *= 2; //s = 60 * 2 = 120
+
+int d = 80 ; 
+cout << d << "\n";
+//d = d / 2; 
+d /= 2;
+
+int  c = 40; 
+cout << c << "\n";
+//c = c % 4;
+c %= 4 ;
+///=====================//10مشروع حساب عدد النقرات وعدد الزيارات
+
+int like = 0;
+cout << like++ << "\n";//0
+cout << like << "\n";//1
+
+int likes = 0; 
+cout << likes-- << "\n";//0
+cout << likes << "\n";//-1
+
+int likeh = 0;
+cout << ++likeh << "\n";//1
+cout << likeh << "\n";//1
+
+int liked = 0; 
+cout << --liked << "\n";//-1
+cout << liked << "\n";//-1*/
+//==========================////11
+
+int get;
+
+cin >> get;
+if(get < 20)
+{
+    cout << " yue\n";
+}
+if(get > 20)
+{
+cout << "no\n";
+}
+
+return 0;
+}
---
+++ oto.js
@@ -0,0 +1,57 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//=========================
+ /* let y = "saed";
+let d = "hi saed";
+let h = "hi saed osama";
+
+document.write( `${y} /
+${d}/
+    ${h}`);
+    let saed = `
+    <div>
+    <div>
+    <h1> saed</h1>
+    <p> saed osama</p>
+    </div>
+    </div>
+    `;
+    console.log(saed);
+    //============================
+let a ={ pagTitle : "Elzero", pagDescription : "Elzero Web School", pagDate:"25/10"};
+let card=`<div>
+<h3>Hello ${a.pagTitle}</h3>
+<p>${a.pagDescription}</p>
+<span>${a.pagDate}</span>
+</div>`;
+document.write(card.repeat(4));
+//=================================
+let e ="90";
+let v = 12;
+let s = true;
+
+console.log(+e + v + s);
+//============================
+var drf = "saed";
+
+console.log("drf");
+
+A1.innerHTML = "hi saed";*/
